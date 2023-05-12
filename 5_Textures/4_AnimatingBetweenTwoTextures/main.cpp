92f8a27d01dc04c21084f52da0d503fd27341d30
adravRevorg <125839825+adravRevorg@users.noreply.github.com>
2023-05-12 13:19:14 -0500

    Animate the interpolation between two textures

---
+++ 5_Textures/4_AnimatingBetweenTwoTextures/main.cpp
@@ -0,0 +1,209 @@
+#include <khrplatform.h>
+#include <glad.h>
+#include <GLFW/glfw3.h>
+#include "Shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <iostream>
+using namespace std;
+
+
+//settings
+#define WIDTH 800
+#define HEIGHT 800
+
+GLFWwindow* window;
+
+//Functions
+bool init();
+void createShaders();
+void setup_objects();
+void setupTexture(unsigned int texture, const char* path);
+void renderLoop();
+void display();
+void processInput(GLFWwindow* window);
+void termination();
+
+
+
+
+//SHADERS
+Shader* shader;
+
+//VAO, VBO
+unsigned int VAO, VBO, EBO;
+
+//Texture
+unsigned int texture[2];
+
+
+int main() {
+
+	if (!init()) return -1;
+
+	createShaders();
+
+	setup_objects();
+
+	renderLoop();
+
+	termination();
+}
+
+
+bool init() {
+	//initialization, config
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//window creation
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Hello Window", NULL, NULL);
+	if (!window) {
+		cout << "Window creation failed";
+		glfwTerminate();
+		return false;
+	}
+
+	//context set
+	glfwMakeContextCurrent(window);
+
+	//glad loader
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to load GLAD";
+		return false;
+	}
+	return true;
+}
+
+void createShaders() {
+	shader = new Shader("vertex.glsl", "fragment.glsl");
+}
+
+
+void setup_objects() {
+
+	//vertices(positions and colors) and indices
+	float vertices[] = {
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+	};
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
+
+	//generate
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	//bind for VAO, VBO, EBO
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//share configuation (position and color)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glGenTextures(2, texture);
+	
+	setupTexture(texture[0], "texture1.png");
+	setupTexture(texture[1], "texture2.png");
+
+	shader->use();
+	//setup texture units for the above two
+	shader->setInt("texture1", 0);
+	shader->setInt("texture2", 1);
+
+	
+
+}
+
+void renderLoop() {
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+		display();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+}
+
+void display() {
+
+	glClearColor(0.3f, 0.6f, 0.8f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
+
+	shader->use();
+	shader->setFloat("fraction", (sin(glfwGetTime()) / 2.0f) + 0.5f);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void termination() {
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shader->ID);
+	glfwTerminate();
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void setupTexture(unsigned int texture, const char* path) {
+
+
+	//setp for 1st texture
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+}
+
+
+/* Summary Notes
+- Building on the previous step, add a time uniform for setting fraction of mix in fragment shader
+- Steps:
+	- Changes to fragment shader: Add a uniform fraction, and use it in the mix
+	- Changes to main: Set a uniform variable in the display loop. I' using sine(time) here 
+*/
